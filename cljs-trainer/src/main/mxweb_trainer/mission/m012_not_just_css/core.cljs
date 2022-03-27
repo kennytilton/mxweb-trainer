1482679ffe9d831d7aa16e09bdd06e79cf5fda6f
Kenneth Tilton <kentilton@gmail.com>
2022-03-27 18:53:56 -0400

    looking for solutions

--- cljs-trainer/src/main/mxweb_trainer/mission/m012_not_just_css/core.cljs
+++ cljs-trainer/src/main/mxweb_trainer/mission/m012_not_just_css/core.cljs
@@ -35,82 +35,104 @@
                                           (reset! jid nil))
                                         (mset! me :tick true))
                                      3000))))}
+    ;
+    ; It /is/ just CSS, tbh, but reactive CSS, making dynamic CSS just another part of the app.
+    ;
+    ; Below we work thru all the ways we can handle CSS /in place/, or co-located if you will.
+    ; We start with the null case: just specifying a convential style string:
 
+    (p {:style "background:none; color:dark-gray; font-size:36px; line-height: 1.2em; padding:3px"}
+      "CSS Unleashed")
+
+    ;; Mind you, we can still leverage CSS classes, and now do so declaratively/reactively
     (div {:class (cF (let [_ (mget (mx-par me) :tick)]
                        (conj [:col-row-switcher]
                          ;; this next is brittle. relies on interval not being an even number of seconds!
+                         ;; so if you change the interval ^^^ from 3 secs to two, not good!
                          (if (even? (.getSeconds (js/Date.)))
                            "col" "row"))))}
-        (span "Hi,&nbsp;")
-        (span "Mom!"))
+      (span "Hi,&nbsp;")
+      (span "Mom!"))
 
-    #_#_#_#_ (p {:style "background:white; color:dark-gray; font-size:36px; line-height: 1.2em; padding:3px"}
-      ; straight CSS string style
-      "CSS Unleashed")
-
+    ;; We can stick to CSS style strings, but add reactivity by building them up based on other state.
+    ;; This is a little silly, because the only reactivity is watching the 'tick' boolean, but any app
+    ;; state would work in a real app where styling have to be responsive.
     (p {:style (cF (str "background:cyan;font-size:32px;line-height:1em;padding:9px;"
                      (if (even? (.getSeconds (js/Date.)))
                        "color:blue;" "color:yellow;")
                      (when-not (mget (mx-par me) :tick)
                        ";visibility:hidden")))}
-      ; still a straight style string, but re-built reactivle
+      ; ...or still a straight style string, but re-built reactively
       "Reactive style string")
 
-    (p {:style (cF (do (mget (mx-par me) :tick)
-                       (style-string
-                         (merge {:font-size   "32px"
-                                 :line-height "1.5em"}
-                           (if (even? (.getSeconds (js/Date.)))
-                             {:background :black
-                              :color      :red}
-                             {:background :red
-                              :color      :white})))))}
-      ; maps are more readable and less likely to get CSS syntax botched
+    ;; Editing/building style strings is clumsy and error prone.
+    ;; Here we code a map and convert to a style string with the utility 'style-string'.
+    ;; Note that the map varies reactively.
+    (p {:style (cF (let [_ (mget (mx-par me) :tick)]
+                     (style-string
+                       (merge {:font-size   "24px"
+                               :line-height "1.5em"}
+                         (if (even? (.getSeconds (js/Date.)))
+                           {:background :black
+                            :color      :red}
+                           {:background :red
+                            :color      :white})))))}
       "Reactive style map -> style-string")
 
+    ;; We take the granularity to another level with an msWeb built-in style object so
+    ;; individual style props can have their own derivations, reactive or not.
+    ;; We get a small efficiency win by having mxWeb update just the changed style properties.
+    ;; More helpful is making explicit derivations recognizable.
     (p {:style (cF1 (let [tag-par (mx-par me)]
                       (make-css-inline me
-                        :font-size (cF (prn :fontsize-cache cache)
-                                     "18px")
+                        :font-size "18px"
                         :line-height "1.5em"
+                        :padding "3px"
                         :background (cF (let [secs (.getSeconds (js/Date.))
                                               tick (mget tag-par :tick)]
                                           (if (even? secs) :black :red)))
                         :color (cF (let [tick (mget tag-par :tick)]
                                      (if (even? (.getSeconds (js/Date.)))
                                        :white :black))))))}
-      ; reactive formulas specific to individual style sub-properties
+      ; the mxWeb :mxweb.css/css type supports reactive formulas for individual style sub-properties.
       "mxWeb CSS Style Object, separate reactive style properties")
 
+    ; And here we are just showing off: one CSS property defined as a function of...
+    ; well, that is your mission.
     (timer/clock 100
       ; finally, one style formula dependent on another formula's property
-      (cFonce (prn :building-clock-css!!!!!)
-          (make-css-inline me
-            :font-size "48px"
-            :line-height "1.5em"
-            :padding "6px"
-            :background (cF (let [tt (mget (:tag @me) :tick-time)
-                                  fake-secs (Math/floor (/ (.getTime tt) 100)) ;; tenths of a sec actually
-                                  degrees (mod fake-secs 360)
-                                  sin (Math/sin (/ degrees (/ 180 Math/PI)))
-                                  gray (Math/abs (Math/floor (* sin 255)))
-                                  xgray (pp/cl-format nil "~2,'0X" gray)
-                                  ]
-                              (apply str "#" (repeat 3 xgray))))
+      (cFonce
+        (make-css-inline me
+          :font-size "48px"
+          :line-height "1.5em"
+          :padding "6px"
+          :background (cF (let [tt (mget (:tag @me) :tick-time)
+                                ;; we do not want to wait for real seconds to pass, so we treat tenths as seconds:
+                                fake-secs (Math/floor (/ (.getTime tt) 100))
+                                ;; getTime is the epoch, and we get a cycling value 0 to 359 via mod
+                                degrees (mod fake-secs 360)
+                                ;; to get smooth transition from 359 to 0, we use the sin circle function
+                                ;; on the degrees converted to radians
+                                sin (Math/sin (/ degrees (/ 180 Math/PI)))
+                                ;; and apply the sin to 255 to get our gray value
+                                gray (Math/abs (Math/floor (* sin 255)))
+                                ;; and convert to a hex string
+                                xgray (pp/cl-format nil "~2,'0X" gray)]
+                            (apply str "#" (repeat 3 xgray))))
 
-            ; --- your code here -------------------------------
-            ;  recall:
-            ;    - any formula starts with the :mxweb.css/css object as "me"
-            ;    - the background will be in the form "#XXXXXX", eg "#FF00000" for red except
-            ;    - we are constructing grays, so each "XX" component will be the same.
-            ;    - we can use hex-string-to-int to convert eg "FF" to 255
-            :color (cF (let [bkg (mget me :background)
-                             bkg-gray-xx (subs bkg 1 3)
-                             gray (style/hex-string-to-int bkg-gray-xx)
-                             igray (- 255 gray)
-                             xigray (pp/cl-format nil "~2,'0X" igray)]
-                         (apply str "#" (repeat 3 xigray))))
-            )))))
+          ; --- your code here -------------------------------
+          ;  recall:
+          ;    - any formula starts with the :mxweb.css/css object as "me"
+          ;    - the background will be in the form "#XXXXXX", eg "#FF00000" for red except
+          ;    - we are constructing grays, so each "XX" component will be the same.
+          ;    - we can use hex-string-to-int to convert eg "FF" to 255
+          :color (cF (let [bkg (mget me :background)
+                           bkg-gray-xx (subs bkg 1 3)
+                           gray (style/hex-string-to-int bkg-gray-xx)
+                           igray (- 255 gray)
+                           xigray (pp/cl-format nil "~2,'0X" igray)]
+                       (apply str "#" (repeat 3 xigray))))
+          )))))
 
 
 (defn mission-factory []
